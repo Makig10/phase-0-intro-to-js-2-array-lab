3f95450f465d04a4d3f5c25ecb2b702b2735bebf
Brian <mahuibrian14@gmail.com>
2023-03-21 14:19:38 +0300

    1st commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+function appendCat(name) {
+    const newcats=[...cats,"Broom"];
+    return newcats;
+}
+function prependCat(name) {
+    const newcats=["Arnold",...cats];
+    return newcats;
+}
+function removeLastCat() {
+    const newcats= cats.slice(0,-1);
+    return newcats;
+}
+function removeFirstCat() {
+    const newcats =cats.slice(1);
+    return newcats;
+}
